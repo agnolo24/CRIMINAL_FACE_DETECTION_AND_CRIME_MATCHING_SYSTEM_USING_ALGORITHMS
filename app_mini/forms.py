8c50c83c3f8aa6228d8c37d8e1cfc8e5b910f16f
amaljith <agnolo2024@gmail.com>
2025-04-29 00:39:46 +0530

    header and footer of the public and staff reg form is done

--- app_mini/forms.py
+++ app_mini/forms.py
@@ -36,10 +36,6 @@ class user_registration_form(forms.ModelForm):
     class Meta:
         model = user_registration
         fields = ['fullname', 'contact']
-        widgets = {
-            'fullname' : forms.TextInput(attrs={'class' : 'form-control'}),
-            'contact' : forms.TextInput(attrs={'class' : 'form-control'})
-        }
 
 
 class search_station_form(forms.Form):
